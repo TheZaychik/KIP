1746dceabf0de1b8bfe5b7e495a607aa89734835
Nikita Zaytsev <nikitos945max@gmail.com>
2019-09-25 17:06:39 +0300

    php edition

---
+++ 4 курс ПКС/СайтСтрой/Proj2/js/secret2.js
@@ -0,0 +1,37 @@
+let var1 = 25;
+let var2 = 10;
+let var3 = 8;
+let var4 = 25;
+let var5 = 30;
+
+if (var1 === 25)
+    document.write("А");
+if (var1 > 15)
+    document.write("Ж");
+if (var1 < 33)
+    document.write("К");
+if (var2 === 10)
+    document.write("А");
+if (var1 + var2 === 30)
+    document.write("Д");
+if (var2 - var1 === 7)
+    document.write("Ш");
+if (var3 < 6)
+    document.write("М");
+if ((var3 === 8) && (var1 + var2 === 7))
+    document.write("З");
+if ((var3 - var2) === (var1 + var2))
+    document.write("Н");
+if (var4 === (var1 + var2 + var3))
+    document.write("А");
+if (var4 > ((var2 - var3) * 4))
+    document.write("Н");
+if (var5 === var1 * 4)
+    document.write("Б");
+if (var5 === var2 * 3)
+    document.write("Ь");
+if ((var1 + var2 + var3 + var4 + var5) / 3 > 60)
+    document.write("К");
+
+
+
---
+++ 4 курс ПКС/СайтСтрой/Proj2/login.php
@@ -0,0 +1,4 @@
+<?php
+$login = $_POST['login'];
+$password = $_POST['password'];
+echo "Мвхахаха, ", $login, "! <br> Я - GulkoHacker, украл твой пароль, кстати вот он: ", $password;
